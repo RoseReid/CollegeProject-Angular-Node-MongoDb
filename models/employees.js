18a20d444b7385a384e1636e4bd6af7dfbce5e51
RoseReid <rosemaryalicereid@gmail.com>
2016-11-12 16:09:50 +0100

    test

--- models/employees.js
+++ models/employees.js
@@ -7,7 +7,6 @@ var db  = mongoose.createConnection(url);
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
 	console.log('connected to employee info')
-  // we're connected!
 });
 
 const employeeSchema = new mongoose.Schema({
