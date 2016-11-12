4e1c740593260f846cc5e42903f4dfb0a445404c
RoseReid <rosemaryalicereid@gmail.com>
2016-11-12 16:08:14 +0100

    schema edit

--- models/car.js
+++ models/car.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// const questionInfo = require('./employee.js');
-// const clientInfo = require('./customer.js');
+const questionInfo = require('./employee.js');
+const clientInfo = require('./customer.js');
 
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/usedCarShop';
@@ -9,7 +9,6 @@ var db  = mongoose.createConnection(url);
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
 	console.log('connected to car info')
-  // we're connected!
 });
 
 const carSchema = new mongoose.Schema({
@@ -18,7 +17,7 @@ const carSchema = new mongoose.Schema({
 	year: {type:String, required:true},
 	registrationNumber: {type: String, required: true},
 	customer: [{type: mongoose.Schema.Types.ObjectId, ref: 'customer'}]
-	// employee: [{type: mongoose.Schema.Types.ObjectId, ref: 'employee'}],
+	employee: [{type: mongoose.Schema.Types.ObjectId, ref: 'employee'}],
 }, {timestamps: true});
 
 exports.Model = db.model('car', carSchema);
--- models/employees.js
+++ models/employees.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// const questionInfo = require('./employee.js');
-// const clientInfo = require('./customer.js');
 
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/usedCarShop';
