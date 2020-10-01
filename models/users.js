d5121df448558e9384015992ff1abc179e0836e9
Justin Nel <dogbertuk2000@gmail.com>
2020-10-01 10:16:57 +0200

    Try to avoid usage of var in Node
    There are plenty of places on client side code you might want to make use of a var, but when you are writing code in Node, you really should try avoid using a var and try stick to const/let where possible.

--- models/users.js
+++ models/users.js
@@ -1,26 +1,22 @@
-
 'use strict';
-
-// const questionInfo = require('./employee.js');
-// const clientInfo = require('./customer.js');
 
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/usedCarShop';
-var db  = mongoose.createConnection(url);
+const db  = mongoose.createConnection(url);
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-	console.log('connected to customer info')
+  console.log('connected to customer info')
   // we're connected!
 });
 
-var records = [
+const records = [
     { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
   , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
 ];
 
 exports.findById = function(id, cb) {
   process.nextTick(function() {
-    var idx = id - 1;
+    const idx = id - 1;
     if (records[idx]) {
       cb(null, records[idx]);
     } else {
@@ -31,8 +27,8 @@ exports.findById = function(id, cb) {
 
 exports.findByUsername = function(username, cb) {
   process.nextTick(function() {
-    for (var i = 0, len = records.length; i < len; i++) {
-      var record = records[i];
+    for (let i = 0, len = records.length; i < len; i++) {
+      const record = records[i];
       if (record.username === username) {
         return cb(null, record);
       }
